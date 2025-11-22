c5f37ea85cb8f5346b59b050be3ed8de5c99a461
Zenoviia <tkachenko.zenoviia@lll.kpi.ua>
2025-11-22 19:23:18 +0200

    feat: add Content logic and styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header/Header";
+import Content from "./components/Content/Content";
 
 function App() {
   return (
     <div style={{ margin: "20px" }}>
       <Header />
+      <Content/>
     </div>
   );
 }
---
+++ my-app/src/components/Content/Content.js
@@ -0,0 +1,90 @@
+import React, { Component } from "react";
+import "./Content.css";
+
+class Content extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstState: "initial",
+      secondState: "initial",
+    };
+  }
+
+  toggleFirst = () => {
+    this.setState((prevState) => {
+      if (prevState.firstState === "initial") return { firstState: "active1" };
+      if (prevState.firstState === "active1")
+        return { firstState: "active1-soft" };
+      return { firstState: "active1" };
+    });
+  };
+
+  toggleSecond = () => {
+    this.setState((prevState) => {
+      if (prevState.secondState === "initial")
+        return { secondState: "active2" };
+      if (prevState.secondState === "active2")
+        return { secondState: "active2-soft" };
+      return { secondState: "active2" };
+    });
+  };
+
+  render() {
+    const hobbies = [
+      "Читати книги",
+      "Дивитися серіали",
+      "Дивитися Формулу-1",
+      "Подорожувати",
+      "Готувати",
+    ];
+
+    const favoriteBooks = [
+      '"Heartless" — Elsie Silver',
+      '"До зустрічі з тобою"',
+      '"Опівнічне сонце"',
+    ];
+
+    return (
+      <div className="content-container">
+        <h3>Хобі:</h3>
+        <ul>
+          {hobbies.map((item, index) => {
+            let className = "";
+            let onClick = null;
+
+            if (item === "Дивитися серіали") {
+              className =
+                this.state.firstState !== "initial"
+                  ? this.state.firstState
+                  : "";
+              onClick = this.toggleFirst;
+            }
+
+            if (item === "Дивитися Формулу-1") {
+              className =
+                this.state.secondState !== "initial"
+                  ? this.state.secondState
+                  : "";
+              onClick = this.toggleSecond;
+            }
+
+            return (
+              <li key={index} className={className} onClick={onClick}>
+                {item}
+              </li>
+            );
+          })}
+        </ul>
+
+        <h3>Улюблені книги та фільми:</h3>
+        <ol>
+          {favoriteBooks.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default Content;
